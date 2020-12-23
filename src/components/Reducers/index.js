932c15491b03326539528b2fd014aeb413f8c523
Daniel Imbrea <dani_imbrea@yahoo.com>
2020-12-23 17:55:27 +0200

    init

--- src/components/Reducers/index.js
+++ src/components/Reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import Identity from "./reducerUserIdentity";
+import activeItem from "./activeItemId";
 
 const allReducers = combineReducers({
   identity: Identity,
+  activeItem: activeItem,
 });
 export default allReducers;
